1580a538d84329d81e452b5fd8fe002090a05567
Alona Danylenko <danilen90@gmail.com>
2026-05-03 19:57:31 +0200

    6 tasks

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", () => {
+  const amount = Number(input.value);
+
+  if (amount < 1 || amount > 100) return;
+
+  destroyBoxes();
+  createBoxes(amount);
+  input.value = "";
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10;
+    const div = document.createElement("div");
+
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+
+    fragment.append(div);
+  }
+
+  boxes.append(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
